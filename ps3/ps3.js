8ef464ab57ac3fb675cfd9046bc5e4bec58c6b74
unknown <hunterspack@gmail.com>
2019-10-20 14:16:16 -0400

    first commit

---
+++ ps3/ps3.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  console.log('put functions in here');
+    
+
+}
+    
+function squareNumber(){
+    console.log("square called");
+    let sqNumber = document.getElementById("userSqNumber").value;
+    let sqAnswer = Math.pow(sqNumber, 2);
+    document.getElementById("squareNum").innerHTML = sqAnswer;
+    console.log("the result of squaring", sqNumber, "is", sqAnswer);
+}
+
+function halfNumber(){
+    console.log("half called")
+    let halfNumber = document.getElementById("userHalfNumber").value;
+    let halfAnswer = halfNumber / 2;
+    document.getElementById("halfNum").innerHTML = halfAnswer;
+    console.log("half of", halfNumber, "is", halfAnswer);
+}
