e4f168d031112d5f23ea2c87a7227303d486db24
unknown <hunterspack@gmail.com>
2019-10-26 04:18:48 -0400

    part one of the assignment is done

--- ps3/ps3.js
+++ ps3/ps3.js
@@ -19,3 +19,33 @@ function halfNumber(){
     document.getElementById("halfNum").innerHTML = halfAnswer;
     console.log("half of", halfNumber, "is", halfAnswer);
 }
+
+function percentOf(){
+    console.log("percent called");
+    let number1 = document.getElementById("userPercentNumber1").value;
+    let number2 = document.getElementById("userPercentNumber2").value;
+    let percent = (number1 / number2) * 100;
+    document.getElementById("percentAns").innerHTML = percent;
+    console.log(number1, "is", percent, "% of", number2);
+
+}
+
+function areaOfCircle(){
+    console.log("circle called");
+    let radius = document.getElementById("radius").value;
+    let area = (Math.PI * (Math.pow(radius, 2)));
+    area = (Math.floor(area * 100) / 100);
+    document.getElementById("areaAns").innerHTML = area;
+    console.log("the area of a circle with a radius of", radius, "is", area);
+}
+
+function together(){
+    console.log("final called");
+    let userinput = document.getElementById("userTogetherNum").value;
+    let squaredFinal = Math.pow(userinput, 2);
+    let halfFinal = userinput / 2;
+    let areaFinal = (Math.PI * (Math.pow(halfFinal, 2)));
+    areaFinal =(Math.floor(areaFinal * 100) / 100);
+    let percentFinal = (areaFinal / squaredFinal) * 100;
+    console.log(percentFinal);
+}
